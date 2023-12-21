8935f7250912e751541fdeed3f5cee79bf56f267
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-12-21 14:01:43 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': "https://backend-two-mocha.vercel.app/api"
+      '/api': "https://backend-two-mocha.vercel.app"
     },
   },
   plugins: [react()],
